ebbeffa163dbad599695d0e7a91e34eed89aec08
marley <mmm@marley-2.local>
2023-06-26 18:00:40 +0000

    this is my first commit

---
+++ index.py
@@ -0,0 +1,152 @@
+
+
+
+'''this is to display my name
+name jjfvkdfgkgld
+grgndfkgkgdflmg
+dskfmskldnmsf '''
+
+"""  """
+# print("marley's")
+
+# print('sika')
+
+# x =4
+# y = 5
+
+# print(x+y)
+
+
+
+# personName = "marley"
+# personPhonenumber = 6544564564
+# personWeight = 22.54 
+# _isAlive_ = True
+# isFemale = False
+
+
+# # print(type(personName))
+# print(type(personPhonenumber))
+
+
+
+
+#DATA TYPES
+# STRING  "" ''
+# string = "qwertyuiop"
+
+# print("a")
+# print(type(string))
+
+
+# personName = ('nana addo' ,'jfdjfdhkjgn')
+# print(type(personName)) 
+# print("hello"+ ' ' +personName[0:3])
+
+# FLOAT 342432.454 -453.45
+# print(23.233 +233.3)
+# INTEGER  4564  -454 
+# BOOLEAN  True or False
+
+
+# LIST   []
+# TUPLES  ()
+# DICT  {}
+# SET  {'a',}
+
+
+# mylist = ["apple", "banana", "cherry", "cherry"]
+
+friendOne ='kofi'
+friendTwo ='ama'
+friendThree ='kojo'
+
+# print(friendTwo)
+friends =['kofi','ama','kojo']
+# print(friends)
+# print(type(friends))
+
+# print(len(friends)) 
+
+# print('my first friend is called' , friends[0])
+# print(friends[2])
+# print(friends[1:2])
+# print(friends[:2])
+
+
+fruits = [
+    'apple', 'banana'
+]
+
+
+
+
+
+# print first 20 fruits
+# print(len(fruits[:20]))
+
+# length of items in our fruits varaible
+
+# check the datatype of the fruits varaible
+
+
+# print(fruits[-2])
+
+
+
+"""
+
+Range of Indexes
+You can specify a range of indexes by specifying where to start and where to end the range.
+When specifying a range, the return value will be a new list with the specified items.
+
+
+
+"""
+# print(fruits)
+# fruits[-1] = 'Ghana' 
+# print(fruits)
+
+
+# fruits[0:5] = ["***","***","***","***","***"]
+
+# fruits.insert(2,"qwerty")
+# fruits.append('gari')
+# fruits.append('x')
+# print(fruits)
+ 
+
+
+# thislist = ["apple", "banana", "cherry","apple"]
+# tropical = ["mango", "pineapple", "papaya"]
+
+# print(tropical+thislist)
+
+# thislist.extend(tropical)
+
+
+# thislist.remove("apple") 
+# thislist.pop(0)
+# thislist.pop(0)
+# thislist.clear()
+# del thislist
+
+ 
+# print(thislist)
+
+
+
+
+#  Python - Update Tuples
+fruits= ("apple", "banana", "cherry","mango", "pineapple", "papaya")
+
+# convert tuple into a list
+newFruitlist  = list(fruits)
+
+newFruitlist[2]= "kiwi"
+# print(type(x))
+
+newFruittuple = tuple(newFruitlist)
+
+print(newFruittuple)
+
