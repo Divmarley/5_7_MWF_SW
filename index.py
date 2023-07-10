f9355f35539cbc27e62998b8da403b7d69a973a9
Divmarley <nexted.marley@gmail.com>
2023-07-10 17:32:30 +0000

    classes

--- index.py
+++ index.py
@@ -1,6 +1,3 @@
-
-
-
 # '''this is to display my name
 # name jjfvkdfgkgld
 # grgndfkgkgdflmg
@@ -17,18 +14,15 @@
 # # print(x+y)
 
 
-
 # # personName = "marley"
 # # personPhonenumber = 6544564564
-# # personWeight = 22.54 
+# # personWeight = 22.54
 # # _isAlive_ = True
 # # isFemale = False
 
 
 # # # print(type(personName))
 # # print(type(personPhonenumber))
-
-
 
 
 # #DATA TYPES
@@ -40,12 +34,12 @@
 
 
 # # personName = ('nana addo' ,'jfdjfdhkjgn')
-# # print(type(personName)) 
+# # print(type(personName))
 # # print("hello"+ ' ' +personName[0:3])
 
 # # FLOAT 342432.454 -453.45
 # # print(23.233 +233.3)
-# # INTEGER  4564  -454 
+# # INTEGER  4564  -454
 # # BOOLEAN  True or False
 
 
@@ -66,7 +60,7 @@
 # # print(friends)
 # # print(type(friends))
 
-# # print(len(friends)) 
+# # print(len(friends))
 
 # # print('my first friend is called' , friends[0])
 # # print(friends[2])
@@ -77,9 +71,6 @@
 # fruits = [
 #     'apple', 'banana'
 # ]
-
-
-
 
 
 # # print first 20 fruits
@@ -93,7 +84,6 @@
 # # print(fruits[-2])
 
 
-
 # """
 
 # Range of Indexes
@@ -101,10 +91,9 @@
 # When specifying a range, the return value will be a new list with the specified items.
 
 
-
 # """
 # # print(fruits)
-# # fruits[-1] = 'Ghana' 
+# # fruits[-1] = 'Ghana'
 # # print(fruits)
 
 
@@ -114,7 +103,6 @@
 # # fruits.append('gari')
 # # fruits.append('x')
 # # print(fruits)
- 
 
 
 # # thislist = ["apple", "banana", "cherry","apple"]
@@ -125,16 +113,14 @@
 # # thislist.extend(tropical)
 
 
-# # thislist.remove("apple") 
+# # thislist.remove("apple")
 # # thislist.pop(0)
 # # thislist.pop(0)
 # # thislist.clear()
 # # del thislist
 
- 
+
 # # print(thislist)
-
-
 
 
 # #  Python - Update Tuples
@@ -151,29 +137,28 @@
 # # print(newFruittuple)
 
 
-
-# # Python - Unpack Tuples 
+# # Python - Unpack Tuples
 # # When we create a tuple, we normally assign values to it.
 # #  This is called "packing" a tuple:
 
 
-fruits = (
-    'apple', 'banana', 'orange', 'grape', 'strawberry', 'watermelon', 'mango',
-    'pineapple', 'kiwi', 'pear', 'cherry', 'blueberry', 'raspberry', 'lemon',
-    'lime', 'peach', 'plum', 'apricot', 'pomegranate', 'coconut', 'avocado',
-    'fig', 'guava', 'grapefruit', 'papaya', 'melon', 'cantaloupe', 'passionfruit',
-    'dragonfruit', 'blackberry', 'raspberry', 'blackcurrant', 'cranberry',
-    'gooseberry', 'kiwifruit', 'lychee', 'mangosteen', 'nectarine', 'persimmon',
-    'quince', 'star fruit', 'tangerine', 'apricot', 'boysenberry', 'elderberry',
-    'honeydew', 'jackfruit', 'mulberry', 'olive', 'rhubarb', 'soursop', 'ugli fruit',
-    'yuzu', 'ackee', 'breadfruit', 'cherimoya', 'durian', 'feijoa', 'grapefruit',
-    'kiwano', 'loquat', 'mandarin', 'pawpaw', 'plantain', 'salak', 'santol',
-    'sapodilla', 'tamarillo', 'tamarind', 'ugli fruit', 'watermelon', 'ziziphus',
-    'apricot', 'banana', 'coconut', 'date', 'elderberry', 'fig', 'grape', 'honeydew',
-    'ilama', 'jambul', 'kiwi', 'lime', 'mango', 'nectarine', 'orange', 'papaya',
-    'quince', 'raspberry', 'strawberry', 'tangerine', 'ugli fruit', 'vanilla',
-    'watermelon', 'xigua', 'yangmei', 'zucchini'
-) 
+# fruits = (
+#     'apple', 'banana', 'orange', 'grape', 'strawberry', 'watermelon', 'mango',
+#     'pineapple', 'kiwi', 'pear', 'cherry', 'blueberry', 'raspberry', 'lemon',
+#     'lime', 'peach', 'plum', 'apricot', 'pomegranate', 'coconut', 'avocado',
+#     'fig', 'guava', 'grapefruit', 'papaya', 'melon', 'cantaloupe', 'passionfruit',
+#     'dragonfruit', 'blackberry', 'raspberry', 'blackcurrant', 'cranberry',
+#     'gooseberry', 'kiwifruit', 'lychee', 'mangosteen', 'nectarine', 'persimmon',
+#     'quince', 'star fruit', 'tangerine', 'apricot', 'boysenberry', 'elderberry',
+#     'honeydew', 'jackfruit', 'mulberry', 'olive', 'rhubarb', 'soursop', 'ugli fruit',
+#     'yuzu', 'ackee', 'breadfruit', 'cherimoya', 'durian', 'feijoa', 'grapefruit',
+#     'kiwano', 'loquat', 'mandarin', 'pawpaw', 'plantain', 'salak', 'santol',
+#     'sapodilla', 'tamarillo', 'tamarind', 'ugli fruit', 'watermelon', 'ziziphus',
+#     'apricot', 'banana', 'coconut', 'date', 'elderberry', 'fig', 'grape', 'honeydew',
+#     'ilama', 'jambul', 'kiwi', 'lime', 'mango', 'nectarine', 'orange', 'papaya',
+#     'quince', 'raspberry', 'strawberry', 'tangerine', 'ugli fruit', 'vanilla',
+#     'watermelon', 'xigua', 'yangmei', 'zucchini'
+# )
 
 # # bad ways
 # # a = fruits[0]
@@ -183,21 +168,16 @@ fruits = (
 # # (x,*Y,y,X) = fruits
 
 
-
 # # Using Asterisk*
 # # If the number of variables is less than the number of values,
 # #  you can add an * to the variable name and the values will be assigned to the variable as a list:
 
 # # good approach
 # # (a ,*b,c) = fruits
- 
-# # print(a) 
+
+# # print(a)
 # # print(b)
 
-
- 
-
- 
 
 # # print(fruits.count("banana"))
 # x = fruits.index("tangerine")
@@ -205,8 +185,8 @@ fruits = (
 
 
 # fruits= {1}
- 
- 
+
+
 # # fruits.pop()
 # # fruits.pop()
 # # fruits.pop()
@@ -222,15 +202,11 @@ fruits = (
 # Dictionary
 # Dictionaries are used to store data values in key:value pairs.
 
-# A dictionary is a collection which is ordered*, 
+# A dictionary is a collection which is ordered*,
 # changeable and do not allow duplicates.
 
 
-
-car = {
-    "name": "BMW",
-    "color":"red"
-}
+car = {"name": "BMW", "color": "red"}
 
 # print(type(car))
 # print(car)
@@ -251,15 +227,11 @@ car = {
 # print(my_list)
 
 
-
-
 # def name_function():
 #     print('hello')
- 
- 
+
+
 # name_function()
-
-
 
 
 # def name_function():
@@ -268,23 +240,15 @@ car = {
 # print(name_function())
 
 
-
-
-
 # def display_name(name):
 #     print(name)
 
 
 # display_name()
- 
 
 
-
-
-
-
-# def calc_age(currentYear,yearBorn): 
-#     return  currentYear-yearBorn  
+# def calc_age(currentYear,yearBorn):
+#     return  currentYear-yearBorn
 
 # print(calc_age(2024,1957))
 # print(calc_age(2024,1992))
@@ -298,7 +262,6 @@ car = {
 # print(twitter_username_gen('marley'))
 
 
-
 # (h**2)/m
 # (h*h)/m
 
@@ -308,7 +271,7 @@ car = {
 
 # print(calculate_BMI(1.60,30))
 # print(calculate_BMI(2.3,50))
- 
+
 # def list_children( *children):
 #     return children[-1]
 
@@ -331,17 +294,16 @@ car = {
 # how to create a function with return value(s).
 
 
-# def display_name( yourName="a" ): 
+# def display_name( yourName="a" ):
 #     return "hello" +  " "+ yourName[-1]
 
 # print(display_name())
 
 # def display_greet(greet):
 #     return greet
-    
+
 # print(display_greet("good morning"))
 # print(display_greet("good afteroon "))
-
 
 
 # lambda function
@@ -358,7 +320,6 @@ car = {
 # print(display_greet("good afternoon",'ama'))
 
 
-
 # def cal_age(currentYear,yearBorn):
 #     age = currentYear - yearBorn
 #     return age
@@ -368,8 +329,6 @@ car = {
 # x = lambda currentYear,yearBorn :   currentYear - yearBorn
 
 # x()
-
-
 
 
 # def myfunc(n):
@@ -389,10 +348,10 @@ car = {
 
 # Python Assignment Operators
 
-# y = 50 
+# y = 50
 # x = 10
 # y = 50
- 
+
 # x+=y
 # x-=y  # x = x - y
 # x*=y  # x = x * y
@@ -412,11 +371,11 @@ y = 50
 # print(x < y) #less than #output boolean
 
 # print(5 > 10 or  10 < 10)
-  
 
-y = 40 
+
+y = 40
 x = 19
-const =18 
+const = 18
 
 # if (x > const):
 #     print("Enter club")
@@ -424,13 +383,15 @@ const =18
 #     print("go home")
 
 
+email_bd = "kofikumi64@gmail.com"
+password_bd = "passme123"
+phone_db = "0244404044"
+username_db = "@marley"
 
-email_bd  = "kofikumi64@gmail.com"
-password_bd  = "passme123"
-
-input_email = "kofikumi64@gmail.com"
-input_password  = "passme1233"
- 
+input_email = "kofikumi64@gmail.comd"
+input_password = "passme123"
+input_phone = "0244404044"
+input_username = "@marley2"
 
 # if (input_email == email_bd):
 #     print("email is correct proceed")
@@ -443,11 +404,182 @@ input_password  = "passme1233"
 #     print("password is not correct try again")
 
 
+# if (input_email == email_bd and input_password ==password_bd):
+#     print("login")
+# else:
+#     print("not correct try again")
 
-if (input_email == email_bd and input_password ==password_bd):
-    print("login")
-else:
-    print("not correct try again")
+
+# if (input_email == email_bd or input_phone==phone_db or input_username==username_db):
+#     if(input_email == email_bd):
+#         print(f"email was used  {input_email}")
+#         print("proceed to enter your password")
+#         if (input_password ==password_bd):
+#             print("login")
+#     elif(input_phone==phone_db):
+#         print(f"phone number was used  {input_phone}")
+#         print("proceed to enter your password")
+#         if (input_password ==password_bd):
+#             print("login")
+#     elif( input_username==username_db):
+#         print(f"username was used  {input_username}")
+#         print("proceed to enter your password")
+#         if (input_password ==password_bd):
+#             print("login")
+# else:
+#     print("not correct try again")
+
+# openlabs@rrc
 
 
-if Else
+# a = 200
+# b = 33
+
+
+# if b > a:
+#   print("b is greater than a")
+# else:
+#   print("b is not greater than a")
+
+
+# print("b is greater than a") if b > a else print("b is not greater than a")
+
+
+# thislist = ["apple", "banana", "cherry"]
+
+# if "banana" in thislist:
+#     print('yes')
+# else:
+#     print("no")
+
+
+# Loops
+
+# fruits = (
+#     'apple', 'banana', 'orange', 'grape', 'strawberry', 'watermelon', 'mango',
+#     'pineapple', 'kiwi', 'pear', 'cherry', 'blueberry', 'raspberry', 'lemon',
+#     'lime', 'peach', 'plum', 'apricot', 'pomegranate', 'coconut', 'avocado',
+#     'fig', 'guava', 'grapefruit', 'papaya', 'melon', 'cantaloupe', 'passionfruit',
+#     'dragonfruit', 'blackberry', 'raspberry', 'blackcurrant', 'cranberry',
+#     'gooseberry', 'kiwifruit', 'lychee', 'mangosteen', 'nectarine', 'persimmon',
+#     'quince', 'star fruit', 'tangerine', 'apricot', 'boysenberry', 'elderberry',
+#     'honeydew', 'jackfruit', 'mulberry', 'olive', 'rhubarb', 'soursop', 'ugli fruit',
+#     'yuzu', 'ackee', 'breadfruit', 'cherimoya', 'durian', 'feijoa', 'grapefruit',
+#     'kiwano', 'loquat', 'mandarin', 'pawpaw', 'plantain', 'salak', 'santol',
+#     'sapodilla', 'tamarillo', 'tamarind', 'ugli fruit', 'watermelon', 'ziziphus',
+#     'apricot', 'banana', 'coconut', 'date', 'elderberry', 'fig', 'grape', 'honeydew',
+#     'ilama', 'jambul', 'kiwi', 'lime', 'mango', 'nectarine', 'orange', 'papaya',
+#     'quince', 'raspberry', 'strawberry', 'tangerine', 'ugli fruit', 'vanilla',
+#     'watermelon', 'xigua', 'yangmei', 'zucchini'
+# )
+
+# bad
+# print(fruits[0])
+# print(fruits[1])
+# print(fruits[2])
+# print(fruits[3])
+
+
+# for x in fruits:
+#     print(x )
+
+
+# print(range(6))
+# for x in range(2,6):
+#     pass
+
+
+# i = 1
+# while i < 6:
+#   print(i)
+#   i += 1
+# else:
+#   print("i is no longer less than 6")
+
+personalInfor = [
+  {
+    "name": "marley",
+    "age": 20,
+    "address": {
+      "country": "Ghana",
+      "zip_code": "+233",
+      "phoneNumbers": [
+        7777777, 
+        900000
+        ],
+    },
+    "gender": "male",
+    "occupation": ("developer"),
+  },
+]
+
+ 
+
+# personalInfor[0]['occupation'] ="dancer"
+# print(personalInfor)
+
+
+ 
+
+# personalInfor[0]["address"]["phoneNumbers"] = tuple(personalInfor[0]["address"]["phoneNumbers"])
+# print(personalInfor)
+
+
+
+# r ={"phoneNumbers": [7777777,  900000 ]}
+
+# r["phoneNumbers"]=(7777777,  900000 )
+# print(r)
+
+
+
+# class and objects
+print()
+
+
+class Person():
+  # properties
+  def __init__(self,paraName,paraColor,pheight,pGender):
+    self.name = paraName
+    self.color= paraColor
+    self.height=pheight
+    self.gender = pGender
+
+  # method
+  def can_talk(self):
+    return 'i can talk'
+
+obj = Person("marley","red",5.99,"male")
+obj2 = Person("ama","yellow",2.99,"female")
+obj3 = Person()
+obj4 = Person()
+
+
+
+print(obj.name)
+print(obj.color)
+print(obj.height)
+print(obj.gender)
+print("=======================================")
+
+
+print(obj2.name)
+print(obj2.color)
+print(obj2.height)
+print(obj2.gender)
+ 
+
+
+
+
+
+
+
+
+
+def display_name(name):
+  return name
+
+print(display_name)
+
+ 
