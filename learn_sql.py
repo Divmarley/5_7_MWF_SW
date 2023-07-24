403e3dd321202ed428e750663a572a8904bd5113
Divmarley <nexted.marley@gmail.com>
2023-07-24 19:13:44 +0000

    python sql creating, insert fetch

--- learn_sql.py
+++ learn_sql.py
@@ -1,11 +1,64 @@
 import mysql.connector
 
 mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password=""
+    host = 'localhost',
+    user ="root",
+    password ="",
+    database = "python_mwf_57"
 )
 
-mycursor = mydb.cursor()
+# print(mydb)
 
-mycursor.execute("DROP DATABASE papapapapapa")
+myCursor = mydb.cursor()
+
+
+# myCursor.execute("SHOW DATABASES")
+
+# for database in myCursor:
+#     print(database)
+    
+# creating database
+# myCursor.execute("CREATE DATABASE python_mwf_57")
+
+# creating table
+# myCursor.execute("CREATE TABLE  users(id INT, name VARCHAR(200) )")
+
+
+
+# INSERT DATA INTO the user
+# myCursor.execute("INSERT INTO users(id,name) VALUES (1,'marley')")
+
+# mydb.commit()
+
+
+# another ways 
+# sql = "INSERT INTO users(id,name) VALUES (%s,%s)"
+# val = [
+#     (4,'ama'),
+#     (5,'nii')
+# ]
+# myCursor.executemany(sql,val)
+
+# mydb.commit()
+
+# print(myCursor.rowcount,"data was inserted ")
+
+
+# select all data from table 
+
+myCursor.execute("SELECT * FROM users ")
+
+myresult = myCursor.fetchall()
+
+for user in myresult:
+    print(user)
+
+
+
+# select a particular data from table 
+myCursor.execute("SELECT * FROM users WHERE name ='randy'")
+
+myresult = myCursor.fetchall()
+
+for user in myresult:
+    print(user)
